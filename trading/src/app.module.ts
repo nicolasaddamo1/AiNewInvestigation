71b65e2bdb8becb2bdb283c9106043403cf10be2
Nico <nicolasaddamo1@gmail.com>
2025-02-28 17:47:16 -0300

    ready

--- trading/src/app.module.ts
+++ trading/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TradingModule } from './trading/trading.module';
 import { ConfigModule } from '@nestjs/config';
+import { BuyModule } from './buy/buy.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
     }),
     TradingModule,
+    BuyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
