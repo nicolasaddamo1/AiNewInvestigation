b279092264ae1f30128e0279c71f7ce658e706e5
Nico <nicolasaddamo1@gmail.com>
2025-02-28 16:18:53 -0300

    added interface

--- trading/src/trading/dto/upload-image.dto.ts
+++ trading/src/trading/dto/upload-image.dto.ts
@@ -1,8 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class UploadImageDto {
-    @ApiProperty({ type: 'string', format: 'binary' }) // Para Swagger
     @IsNotEmpty()
     image: any; // El archivo de imagen
 }
